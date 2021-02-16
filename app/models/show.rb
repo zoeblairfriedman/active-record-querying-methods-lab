e325722008da8b3ca2a6a59d507987930b9504c3
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-16 14:33:46 -0500

    commit tree c86ca638a398a0398e6c47bfb4cd2779a9699ad4

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        Show.where("rating == ?", self.highest_rating)[0]
+    end
+
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        Show.where("rating == ?", self.lowest_rating)[0]
+    end
+
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+
+    def self.popular_shows
+        Show.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        Show.order(:name)
+    end
+
+end
